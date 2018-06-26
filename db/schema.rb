712bf72a66d0aa963eb32028de6703bb1e2ff762
Nathan Cooper <ncoop720@yahoo.com>
2018-06-26 15:17:28 -0400

    post and controller resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_06_26_191656) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.integer "parent_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "author"
+    t.text "categories"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
