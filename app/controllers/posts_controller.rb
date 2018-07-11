bdf5d77793808883bdd29efac798dac68ad1a405
Nathan Cooper <ncoop720@yahoo.com>
2018-07-11 13:13:49 -0400

    featured post in main index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
 	def index
-		@posts = Post.all.order(:created_at).reverse
+		@featured_post = Post.last
+		@posts = Post.all.order(:created_at).reverse.drop(1)
 	end
 
 end
