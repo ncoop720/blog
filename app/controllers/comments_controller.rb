57d6e03d1c4c68494300cb07544af31f1c62e689
Nathan Cooper <ncoop720@yahoo.com>
2018-07-19 20:19:13 -0400

    comment controller methods new, create, and find commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+
+	before_action :find_commentable
+
+    def new
+      @comment = Comment.new
+    end
+
+    def create
+      @comment = @commentable.comments.new comment_params
+
+      if @comment.save
+        redirect_to :back, notice: 'Your comment was successfully posted!'
+      else
+        redirect_to :back, notice: "Your comment wasn't posted!"
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+    def find_commentable
+      @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+      @commentable = Post.find_by_id(params[:post_id]) if params[:post_id]
+    end
+
 end
